9cdc34727c74d389e9a5c226ec1ca3692a4ea7aa
Sasipim <iamsasipim@gmail.com>
2023-04-07 20:05:37 +0700

    connect db

--- cloud-final-server/src/app.module.ts
+++ cloud-final-server/src/app.module.ts
@@ -2,17 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RequestHistory } from './schema/requestHistory.entity';
+import { Score } from './schema/score.entity';
+import { ScoreBoard } from './schema/scoreBoard.entity';
+
+require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      host: 'http://ec2-18-191-47-43.us-east-2.compute.amazonaws.com',
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [RequestHistory, Score, ScoreBoard],
       synchronize: true,
     }),
   ],
