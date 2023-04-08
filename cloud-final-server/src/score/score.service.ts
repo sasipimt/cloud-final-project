eec369e4070a759eb31df1eb9c55a937d3c0f7e9
Sasipim <iamsasipim@gmail.com>
2023-04-08 12:53:54 +0700

    Update score.service.ts

--- cloud-final-server/src/score/score.service.ts
+++ cloud-final-server/src/score/score.service.ts
@@ -7,6 +7,10 @@ import { Repository, DeleteResult, UpdateResult } from 'typeorm';
 import { AudioRequestDto } from '../dto/audioRequest.dto';
 import { ScoreRequestDto } from '../dto/scoreRequest.dto';
 
+const speech = require('@google-cloud/speech');
+const line = require('@line/bot-sdk');
+require('dotenv').config();
+
 @Injectable()
 export class ScoreService {
   constructor(
@@ -37,6 +41,47 @@ export class ScoreService {
   }
 
   async getScore(scoreRequestDto: ScoreRequestDto): Promise<string> {
+    const lineClient = new line.Client({
+      channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    });
+    let audioBytes;
+    let hasError = false;
+
+    lineClient.getMessageContent(scoreRequestDto.messageId).then((stream) => {
+      stream.on('data', (chunk) => {
+        audioBytes = chunk.toString('base64');
+      });
+      stream.on('error', (err) => {
+        // error handling
+        hasError = true;
+        console.log(err);
+      });
+    });
+
+    if (!hasError) {
+      const client = new speech.SpeechClient();
+      const audio = {
+        content: audioBytes,
+      };
+
+      const config = {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 24000,
+        languageCode: 'en-US',
+      };
+
+      const request = {
+        audio,
+        config,
+      };
+
+      const [response] = await client.recognize(request);
+      const transcription = response.results
+        .map((result) => result.alternatives[0].transcript)
+        .join('\n');
+      console.log(`Transcription: ${transcription}`);
+    }
+
     return 'Hello World!';
   }
 
