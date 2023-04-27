0e414f193529841485983c574efba86ed0ca7565
Sasipim <iamsasipim@gmail.com>
2023-04-27 18:07:26 +0700

    Update score.service.ts

--- cloud-final-server/src/score/score.service.ts
+++ cloud-final-server/src/score/score.service.ts
@@ -118,7 +118,8 @@ export class ScoreService {
     // Instantiates a client.
 
     // The path to the remote audio file.
-    const gcsUri = 'gs://cloud-final-project-fung/Test_thai.wav';
+    const gcsUri =
+      'https://storage.googleapis.com/cloud-final-project-fung/Test_thai.wav';
 
     // Transcribes your audio file using the specified configuration and prints the transcription.
     async function transcribeSpeech() {
