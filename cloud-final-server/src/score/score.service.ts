0c116eaed4c82a4b508726e34db7bc0dde9585d6
Sasipim <iamsasipim@gmail.com>
2023-04-29 12:31:00 +0700

    Update score.service.ts

--- cloud-final-server/src/score/score.service.ts
+++ cloud-final-server/src/score/score.service.ts
@@ -22,7 +22,7 @@ import { Client } from '@line/bot-sdk';
 
 const speech = require('@google-cloud/speech');
 const line = require('@line/bot-sdk');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
@@ -97,9 +97,9 @@ export class ScoreService {
     const stream = await lineClient.getMessageContent(
       scoreRequestDto.messageId,
     );
-    await stream.on('data', (chunk) => {
-      fs.writeFile(`${fileName}.m4a`, chunk);
-      fs.writeFile(`${fileName}.wav`, 'a');
+    await stream.on('data', async (chunk) => {
+      await fs.writeFile(`${fileName}.m4a`, chunk);
+      await fs.writeFile(`${fileName}.wav`, 'a');
     });
 
     // await stream.on('end', async () => {
