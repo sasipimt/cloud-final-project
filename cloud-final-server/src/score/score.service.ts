8e629efd935bb01d20da2facf985ac0e5e1ca2e2
Sasipim <iamsasipim@gmail.com>
2023-04-29 11:22:40 +0700

    Update score.service.ts

--- cloud-final-server/src/score/score.service.ts
+++ cloud-final-server/src/score/score.service.ts
@@ -97,9 +97,9 @@ export class ScoreService {
     const stream = await lineClient.getMessageContent(
       scoreRequestDto.messageId,
     );
-    await stream.on('data', (chunk) => {
-      fs.writeFileSync(`${fileName}.m4a`, chunk);
-      fs.writeFileSync(`${fileName}.wav`, 'a');
+    await stream.on('data', async (chunk) => {
+      await fs.writeFileSync(`${fileName}.m4a`, chunk);
+      await fs.writeFileSync(`${fileName}.wav`, 'a');
     });
 
     await stream.on('end', async () => {
